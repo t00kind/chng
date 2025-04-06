4d6cd0ea825daf8f0b6a482490a1d3818e574862
ministr <you@example.com>
2025-04-06 14:17:23 +0300

    real :

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import Akatski from "./ui/Dawn";
+import What from "./ui/cmps/What";
+import Logo from "./ui/cmps/Logo";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
@@ -14,42 +15,80 @@ export default function Home() {
   ScrollTrigger.config({ ignoreMobileResize: true });
 
   const [r, setR] = useState('И ЕСЛИ УЖ МЕНЯТЬ')
+  const [hot, setH] = useState('');
+  const [next, setN] = useState('');
+  const [chng, setC] = useState('change');
+  const [m, setM] = useState('');
+  const [f, setF] = useState(false);
 
   useGSAP(() => {
     let am = ScrollTrigger.maxScroll(window)*(1/2)
     const tl = gsap.timeline();
+    
     tl
-    .to("#part1", {scrollTrigger: {
-      trigger: "#part1",
-      start: "top center",
-      end: "center center",
-      pin: true,
-      scrub: true,
-
-  },
-      opacity: 1})
+      .to("#logo", {
+        scrollTrigger: {
+          trigger: "#mr_penis",
+          start: "center center",
+          end: "bottom top",
+          scrub: 0.3
+  
+        }, 
+        top: "2.3%",
+        fontSize: '0.67em'
+      })
+      .to("#say", {
+        scrollTrigger: {
+          trigger: '#say',
+          start: "center 50%",
+          end: () => "+=" + am,
+          pin: "#say",
+          toggleActions:"restart complete reverse reset",
+          scrub: true,
+          onEnter: () => {setM("КТО?ТЫ"),setH("your"), setN("life"), setC("change")},
+          onEnterBack: () => {setC("change")},
+          onLeave: () => {setN(""), setH(""), setC("changing"), setF(true)},
+          onUpdate: ({progress}) => {
+            if (progress > 0.3 && progress < 0.6) {
+            setM("РУШЬ РАМКИ"); 
+            setH("your"), setN("mind") } else if ((progress > 0.7) && (!f)) {
+              setM("МЕНЯЙ МИР");
+              setH("the");
+              setN("world")
+            } else if (progress < 0.3) {
+              setM("КТО?ТЫ"),setH("your"), setN("life")
+            }
+          },
+          onLeaveBack: () => {setH(""), setN(""), setM(""), setC("change")},
+        },
+        ease: "power1.inOut",
+      })
+      .to("#part1", {scrollTrigger: {
+        trigger: "#part1",
+        start: "top center",
+        end: "center center",
+        pin: true,
+        scrub: true,
+        onLeave: () => {setC("CHNG")}
+  
+    },
+        opacity: 1})
       .to("#part2", {scrollTrigger: {
         trigger: "#part2",
         start: "top center",
         end: "center center",
         pin: true,
         scrub: true,
+        onEnter: () => {setR("二")}
   
-    },
-        opacity: 1})
-        .to("#part2", {scrollTrigger: {
-          trigger: "#part2",
-          start: "top center",
-          end: "center center",
-          pin: true,
-          scrub: true,
-          onEnter: () => {setR("二")}
-    
-      }}); 
+    }})
   });
   return (
       <main className='main'>
-          <Akatski />
+            <Logo c={chng} h={hot} n={next}/>
+            <div className="hig"></div>
+            <What ms={m}/>
+            <div className="hig"></div>
           <h1 id='part1'>ТВОЙ МИР - ТЫ</h1>
           <Image
               src={zangetsu}
@@ -57,22 +96,18 @@ export default function Home() {
               height={500}
               alt='Old Man zangetsu'
             />
-          <h1>ВЕСЬ МИР</h1>
-          <h1>ВСЯ ЖИЗНЬ</h1>
-          <h1>СХОДЯТСЯ В ОДНУ ТОЧКУ</h1>
-          <h1>ОЧНИСЬ И ВЗЛЯНИ НА СВОЮ ДУШУ!</h1>
-          <h1>ОЩУТИ</h1>
-          <h1>РАССЕКИ, НИЧТОЖНЫЕ ИЛЛЮЗИИ</h1>
-          <h1>ПОВЕРЬ В СВОИ СИЛЫ</h1>
-          <h1>НЕ ОСТАНАВЛИВАЙСЯ!</h1>
-          <h1>ТОЛЬКО ТЫ</h1>
-          <h1>ТОЛЬКО В ТВОИХ РУКАХ</h1>
-          <h1>МЕЧТЫ, МЫСЛИ, ТЫ</h1>
+          <h1>ОЧНИСЬ И ВЗЛЯНИ НА РЕАЛЬНОСТЬ</h1>
+          <h1>ЭТОТ МИР</h1>
           <h1>ВНУТРИ</h1>
+          <h1>ПОКОРИТЬ ЭТО НЕБО</h1>
+          <h1>РАССЕЧЬ</h1>
+          <h1>РАЗРУШИТЬ ЭТУ ЛОЖЬ</h1>
+          <h1>ИБО Я СИЛЬНЕЙШИЙ ELDR AAAA </h1>
           <div className="hig"></div>
           <h1 id='part2'>ТЫ - МИР</h1>
           <p>СОВЕРШАЯ БОЛЬШИЕ И МАЛЫЕ ДЕЙСТВИЯ</p>
           <p>НЕПРИМЕННО ВЕРШИШЬ ИЗМЕНЕНИЯ ВОКРУГ</p>
+          <h1>ПУТЬ БЕСЧИСЛЕННЫХ ИЗМЕНЕНИЙ, ПРИБЛИЖАЯСЬ К СОВЕРШЕНСТВУ</h1>
           <h1>И СЕРДЦА</h1>
           <p>ТЫСЯЧЕ СОЛНЦ</p>
           <h1>СЛОВНО БУСЫ НИТЬЮ</h1>
--- app/ui/cmps/Logo.tsx
+++ app/ui/cmps/Logo.tsx
@@ -3,18 +3,20 @@ import localFont from 'next/font/local';
 const b = localFont({src: "../fonts/trans.woff2"});
 import { JetBrains_Mono } from "next/font/google";
 const g = JetBrains_Mono({ weight: ['400'], subsets: ['cyrillic', 'latin']});
-
+let kk = '3em'
 const Logo = ({c, h, n}: {c: any, h: any, n: any;}) => {
     let res;
-    if (c === "ПЕРЕМЕНЫ ВНУТРИ") {
+    if (c === "ПЕРЕМЕНЫ ВНУТРИ" || c === "CHNG") {
         res = g.className
+        kk = '3.5em'
     } else {
         res = b.className
+        kk = '3em'
     }
 
     return (
         <div id='logo'>
-            <h1 id="mr_penis" className={res}>{c}<span className='bruh'>{h}{n}</span></h1>
+            <h1 id="mr_penis" style={{fontSize: kk}} className={res}>{c}<span className='bruh'>{h}{n}</span></h1>
         </div>
     )
 }
